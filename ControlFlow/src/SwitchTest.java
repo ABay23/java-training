25fbd4d3b0936c2c7a44c3837efa0d303cabb106
ABay23 <alejandrobay23@gmail.com>
2023-12-24 00:38:30 -0500

    Switch Statements

--- ControlFlow/src/SwitchTest.java
+++ ControlFlow/src/SwitchTest.java
@@ -1,20 +1,38 @@
 public class SwitchTest {
     public static void main(String [] args){
-        int switchValue = 5;
+        int switchValue = 6;
 
-        switch(switchValue){
-            case 1:
-                System.out.println("Value was 1");
-                break;
-            case 2:
-                System.out.println("Value was 2");
-                break;
-            case 3:
-                System.out.println("Caso value 3");
-                break;
-            default:
-                System.out.println("Basic answer@");
-                break;
+        switch (switchValue) {
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
+                System.out.println("Caso value 3 or 4 or 5");
+                System.out.println("The total value is: " + switchValue);
+            }
+            default -> System.out.println("Basic answer@");
         }
+        String month = "October";
+        System.out.println(month + " is in the " + quarter(month) + " quarter");
+    }
+    public static String quarter(String month){
+        switch(month) {
+            case "January":
+            case "February":
+            case "March":
+                return "1st";
+            case "April":
+            case "May":
+            case "June":
+                return "2nd";
+            case "July":
+            case "August":
+            case "September":
+                return "3rd";
+            case "October":
+            case "November":
+            case "December" :
+                return "4th";
+        }
+        return "Bad";
     }
 }
