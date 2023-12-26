5cef9354d0f66e982e7656932f845f4adabc16a1
ABay23 <alejandrobay23@gmail.com>
2023-12-25 23:28:28 -0500

    New test

--- CalculateInterest/src/Main.java
+++ CalculateInterest/src/Main.java
@@ -4,6 +4,10 @@ public class Main {
         for (int i = 0; i < counter; i ++){
             System.out.println(i);
         }
+        for (double rate = 2.0; rate <= 5.0; rate ++) {
+            System.out.println("10,000 at " + rate + " interest = " + calculateInterest(10000.0, rate));
+
+        }
 
     }
     public static double calculateInterest(double amount, double interestRate){
