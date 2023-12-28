a6dd5122d2c0e977277acb6171add9aac7901b31
ABay23 <alejandrobay23@gmail.com>
2023-12-28 17:40:23 -0500

    New Build

--- Composition/src/Main.java
+++ Composition/src/Main.java
@@ -1,3 +1,12 @@
 public class Main {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        ComputerCase theCase = new ComputerCase("2208", "Dell", "240");
+        Monitor theMonitor = new Monitor("27 inch beast", "Acer", 27, "2540 x 1440");
+        MotherBoard theMotherboard = new MotherBoard("2030","Asus", 4, 6, "Bi-call" );
+        PersonalComputer thePC = new PersonalComputer("2208", "Dell", theCase, theMonitor, theMotherboard);
+
+        thePC.getMonitor().drawPixelAt(10,10, "red");
+        thePC.getMotherBoard().loadProgram("Windows OS");
+        thePC.getComputerCase().pressPowerButton();
+    }
 }
