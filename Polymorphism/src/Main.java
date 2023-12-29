4b93e84cc31edc700720f9882b9ff1658ab912c9
ABay23 <alejandrobay23@gmail.com>
2023-12-29 00:08:19 -0500

    New class

--- Polymorphism/src/Main.java
+++ Polymorphism/src/Main.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
 
-        Movie newMovie = Movie.getMovie("Science Fiction","Star Wars");
-        newMovie.watchMovie();
+//        Movie newMovie = Movie.getMovie("Science Fiction","Star Wars");
+//
+        Scanner s = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter type A- for Adventure, C- for Comedy, S- for Science Fiction and Q- to quit: ");
+            String type = s.nextLine();
+            if ("Qq".contains(type)){
+                break;
+            }
+            System.out.print("Enter Movie Title: ");
+            String title = s.nextLine();
+            Movie movie = Movie.getMovie(type, title);
+            movie.watchMovie();
+        }
+
     }
 }
