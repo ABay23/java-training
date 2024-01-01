0e5f0ab81624bf24e0eee0a8b3e2227d75e0c483
ABay23 <alejandrobay23@gmail.com>
2024-01-01 13:36:01 -0500

    Setup for List and Data

--- SetsMaps2/src/Main.java
+++ SetsMaps2/src/Main.java
@@ -1,3 +1,20 @@
+import java.util.Collection;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        List<Contact> emails = ContactData.getData("email");
+        List<Contact> phones = ContactData.getData("phone");
+        printData("Email List", emails);
+        printData("Phone List", phones);
+
+    }
+
+    public static void printData(String header, Collection<Contact> contacts){
+        System.out.println("------------------------------------");
+        System.out.println(header);
+        System.out.println("------------------------------------");
+        contacts.forEach(System.out ::println);
+    }
 }
+
