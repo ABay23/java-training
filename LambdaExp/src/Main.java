e029ca5ca86e997a2c3d05d0e3be2e5a82bc9c60
ABay23 <alejandrobay23@gmail.com>
2024-01-07 19:00:26 -0500

    Adding abstract exp

--- LambdaExp/src/Main.java
+++ LambdaExp/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
             return firstName + " " + lastName;
         }
     }
-    public static void main(String[] args){
+    public static void main(String[] args) {
         List<Person> people = new ArrayList<>(Arrays.asList(
                 new Main.Person("Lucy", "Van Pelt"),
                 new Person("Sally", "Brown"),
@@ -21,20 +21,32 @@ public class Main {
         ));
 
         //* Using Anonymous CLass
-        var comparatorLastName = new Comparator<Person>(){
+        var comparatorLastName = new Comparator<Person>() {
 
             @Override
             public int compare(Person o1, Person o2) {
                 return o1.lastName().compareTo(o2.lastName());
             }
         };
-        people.sort(new Comparator<Person>(){
+        people.sort((o1, o2) -> o1.lastName().compareTo(o2.lastName()));
+        System.out.println(people);
 
+        interface EnhancedComparator<T> extends Comparator<T> {
+            int secondLevel(T o1, T o2);
+        }
+        var comparatorMixed = new EnhancedComparator<Person>(){
             @Override
             public int compare(Person o1, Person o2) {
-                return o1.lastName().compareTo(o2.lastName());
+                int result = o1.lastName().compareTo(o2.lastName());
+                return (result == 0 ? secondLevel(o1, o2) : result);
             }
-        });
+
+            @Override
+            public int secondLevel(Person o1, Person o2) {
+                return o1.firstName().compareTo(o2.firstName());
+            }
+        };
+        people.sort(comparatorMixed);
         System.out.println(people);
     }
 }
