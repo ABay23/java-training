be4a9adf2bf39f747e5f98de3da0588bb2e1a1ee
ABay23 <alejandrobay23@gmail.com>
2024-01-08 12:44:42 -0500

    New Lambda

--- Lambda2/src/Main.java
+++ Lambda2/src/Main.java
@@ -14,6 +14,8 @@ public class Main {
         list.forEach((s)-> System.out.println(s));
 
         int result = calculator((a, b) -> a + b, 5, 7);
+        var result2 = calculator((a, b)-> a / b , 200.5, 3.2);
+        var result3 = calculator((a, b) -> a / b, 500.1, 2.0);
     }
     public static<T> T calculator(Operation<T> function, T value1, T value2 ){
         T result = function.operate( value1,  value2);
